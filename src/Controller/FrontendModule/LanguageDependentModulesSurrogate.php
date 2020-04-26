d7484766d8ee27da149615a39df84245f49dbbb1
David Greminger <david.greminger@1up.io>
2020-04-26 13:35:31 +0200

    Add frontend module

---
+++ src/Controller/FrontendModule/LanguageDependentModulesSurrogate.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Oneup\Contao\LanguageDependentModulesBundle\Controller\FrontendModule;
+
+use Contao\CoreBundle\Controller\FrontendModule\AbstractFrontendModuleController;
+use Contao\ModuleModel;
+use Contao\StringUtil;
+use Contao\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class LanguageDependentModulesSurrogate extends AbstractFrontendModuleController
+{
+    protected function getResponse(Template $template, ModuleModel $model, Request $request): ?Response
+    {
+        $locale = $request->getLocale();
+        $modules = StringUtil::deserialize($model->modules);
+
+        $template->surrogate = null;
+
+        if (\is_array($modules) && \array_key_exists($locale, $modules)) {
+            $template->surrogate = (int) $modules[$locale];
+        }
+
+        return $template->getResponse();
+    }
+}
